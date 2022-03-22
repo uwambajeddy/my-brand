cc457d29f71881ded5e2700c3f0622ebf4b10e96
eddie leftie <uwambajeddy@gmail.com>
2022-03-22 21:52:09 +0200

    animation

--- js/main.js
+++ js/main.js
@@ -2,6 +2,19 @@ const bar_a = document.querySelector(".icon-bar-a");
 const bar_b = document.querySelector(".icon-bar-b");
 const navbar = document.querySelector(".navbar-toggler");
 const menu = document.querySelector(".navbar-nav");
+const progressBara = document.querySelector(".progress-bar-a");
+const progressBarb = document.querySelector(".progress-bar-b");
+const progressBarc = document.querySelector(".progress-bar-c");
+const progressBard = document.querySelector(".progress-bar-d")
+const progressBare = document.querySelector(".progress-bar-e");
+
+window.addEventListener("scroll",()=>{
+    progressBara.style.width = "85%";
+progressBarb.style.width = "90%";
+progressBarc.style.width = "70%";
+progressBard.style.width = "95%";
+progressBare.style.width = "75%";
+})
 
 navbar.addEventListener("click",()=>{
     if(bar_a.style.marginLeft == "10px"){
