549eab9284006e02c203d8b84a97b39581fb001a
eddie leftie <uwambajeddy@gmail.com>
2022-03-24 17:18:53 +0200

    modal

--- js/main-dash.js
+++ js/main-dash.js
@@ -2,6 +2,10 @@ const menu = document.querySelector(".menu_banner_area");
 const toggler = document.querySelector(".navbar-toggler");
 const bar_a = document.querySelector(".icon-bar-a");
 const bar_b = document.querySelector(".icon-bar-b");
+const modal_block = document.querySelector(".model_block");
+const modal = document.querySelector(".modal_banner_area");
+const modal_add = document.querySelector(".add_btn");
+const modal_update = document.querySelectorAll(".update_btn");
 
 toggler.addEventListener("click",()=>{
     if(menu.style.marginLeft == "-258px"){
@@ -12,3 +16,33 @@ toggler.addEventListener("click",()=>{
     }
    
 })
+modal_block.addEventListener("click",()=>{
+    modal.style.top = "-500px";
+    modal_block.style.display ="none";
+   
+});
+
+/* modal_update.map(update =>{
+    update.addEventListener("click",()=>{
+        if(modal.style.top == "-500px"){
+            modal_block.style.display ="block";
+            modal.style.top = "50%";
+        }else{
+            menu.style.marginLeft = "-500px";
+            modal_block.style.display ="block";
+        }
+       
+    });
+    
+}) */
+
+modal_add.addEventListener("click",()=>{
+    if(modal.style.top == "-500px"){
+        modal_block.style.display ="block";
+        modal.style.top = "50%";
+    }else{
+        modal.style.top = "-500px";
+        modal_block.style.display ="none";
+    }
+   
+});
