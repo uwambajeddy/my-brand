7ebe15e7cceba267a71169fdbb8ff2c6e4cffab5
eddie leftie <59047760+uwambajeddy@users.noreply.github.com>
2022-04-22 11:01:21 +0200

    make it better

--- util/email.js
+++ util/email.js
@@ -81,7 +81,7 @@ const sendEmail = async options => {
   });
 
   const mailOptions = {
-    from: 'Uwambaje eddy <uwambajeddy@gmail.com>',
+    from: 'Uwambaje Eddy <uwambajeddy@gmail.com>',
     to: options.email,
     subject: options.subject,
     text: options.message
