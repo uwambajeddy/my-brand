ab18ebba3e8daef45098ba5c27909c33c67af787
eddie leftie <59047760+uwambajeddy@users.noreply.github.com>
2022-04-22 15:42:28 +0200

    make it better

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ import { fileURLToPath } from 'url';
 import { dirname } from 'path';
 
 const __filename = fileURLToPath( import.meta.url );
-const __dirname = dirname(__filename);
+const __dirname = dirname(__filename); 
 
 const { urlencoded, json } = bodyParser; 
 
