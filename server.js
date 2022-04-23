fd8b63fed16ac35081ba61388d2aa0e5bcc13435
eddie leftie <59047760+uwambajeddy@users.noreply.github.com>
2022-04-23 12:49:37 +0200

    H18 handler

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ import app from './app.js';
 
 process.on('uncaughtException', err => {
   console.log('UNCAUGHT EXCEPTION! 💥 Shutting down...');
-  console.log(err.name, err.message);
+  console.log(err.name, err.message, err.stack);
   process.exit(1);
 });
 
