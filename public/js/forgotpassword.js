86e6d07bb5645751783fcd260de1a5e495f43335
eddie leftie <59047760+uwambajeddy@users.noreply.github.com>
2022-05-04 12:31:02 +0200

    make it better

--- public/js/forgotpassword.js
+++ public/js/forgotpassword.js
@@ -3,27 +3,26 @@
 const contactform = document.querySelector('.contact_form');
 const disibleControl = document.querySelector('.disible-control');
 
-contactform.addEventListener('submit', e => {
+contactform.addEventListener('submit', async (e) => {
   e.preventDefault();
 
   const email = document.querySelector('#email').value;
 
-  if ( email == '' ) {
-   popup(warning,"Please fill empty field !!");
-      
+  if (email == '') {
+    popup(warning, 'Please fill empty field !!');
+
     return 0;
   }
 
   disibleControl.style.display = 'block';
   try {
     await axios.post('/api/v1/user/forgotpassword', {
-      email
+      email,
     });
-    popup(success, "Reset token was sent to your Email address");
+    popup(success, 'Reset token was sent to your Email address');
     disibleControl.style.display = 'none';
   } catch (error) {
     console.log(error);
     popup(failure, `${error.response.data.message}`);
   }
-
 });
