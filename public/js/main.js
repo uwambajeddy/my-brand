56c34bdf7924970cd0c5c0a573f0e768a44b06d7
eddie leftie <59047760+uwambajeddy@users.noreply.github.com>
2022-05-04 09:33:59 +0200

    make it better

--- public/js/main.js
+++ public/js/main.js
@@ -9,6 +9,20 @@ const progressBarc = document.querySelector('.progress-bar-c');
 const progressBard = document.querySelector('.progress-bar-d');
 const progressBare = document.querySelector('.progress-bar-e');
 
+logout.addEventListener('click', async (e) => {
+  e.preventDefault();
+  try {
+    await axios.get('/api/v1/user/logout');
+    popup(success, 'Logged out successfully');
+    setTimeout(() => {
+      location.reload();
+    }, 3000);
+  } catch (error) {
+    console.log(error);
+    popup(failure, `${error.response.data.message}`);
+  }
+});
+
 window.addEventListener('scroll', () => {
   progressBara.style.width = '85%';
   progressBarb.style.width = '90%';
@@ -34,19 +48,5 @@ navbar.addEventListener('click', () => {
   } else {
     menu.style.opacity = '1';
     menu.style.height = '320px';
-  }
-});
-
-logout.addEventListener('click', async (e) => {
-  e.preventDefault();
-  try {
-    await axios.get('/api/v1/user/logout');
-    popup(success, 'Logged out successfully');
-    setTimeout(() => {
-      location.reload();
-    }, 3000);
-  } catch (error) {
-    console.log(error);
-    popup(failure, `${error.response.data.message}`);
   }
 });
